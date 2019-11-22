a64719ac37b13e8212f60abb291a795ee849ee61
Kumar Shubam <ks92217@gmail.com>
2019-11-22 20:11:27 +0530

    Some improvements in map

--- app/src/main/java/com/krshubham/mealsonwheels/ui/LocationDetail.kt
+++ app/src/main/java/com/krshubham/mealsonwheels/ui/LocationDetail.kt
@@ -1,23 +1,29 @@
 package com.krshubham.mealsonwheels.ui
 
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapsInitializer
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.krshubham.mealsonwheels.R
 import kotlinx.android.synthetic.main.activity_location_detail.*
+import java.util.*
 
 class LocationDetail : AppCompatActivity() {
 
 
     private lateinit var position: LatLng
+    private lateinit var geocoder: Geocoder
+    private lateinit var addresses: List<Address>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_location_detail)
+
+        geocoder = Geocoder(this, Locale.getDefault())
 
         position = LatLng(intent.getDoubleExtra("lat", 0.0), intent.getDoubleExtra("lng", 0.0))
         with(mapView) {
@@ -28,6 +34,7 @@ class LocationDetail : AppCompatActivity() {
 
                 MapsInitializer.initialize(applicationContext)
                 setMapLocation(it)
+
             }
         }
     }
@@ -37,9 +44,20 @@ class LocationDetail : AppCompatActivity() {
         with(map) {
 
             animateCamera(CameraUpdateFactory.newLatLngZoom(position,17f))
-            uiSettings.setAllGesturesEnabled(true)
-            addMarker(MarkerOptions().position(position).draggable(true).icon(BitmapDescriptorFactory.defaultMarker()))
             mapType = GoogleMap.MAP_TYPE_NORMAL
+
+            isMyLocationEnabled = true
+
+            setOnCameraIdleListener {
+
+                var a = cameraPosition.target.latitude
+                var b = cameraPosition.target.longitude
+
+                animateCamera(CameraUpdateFactory.newLatLngZoom(cameraPosition.target,17f))
+                addresses = geocoder.getFromLocation(cameraPosition.target.latitude,cameraPosition.target.latitude,1)
+                address.text = addresses[0].locality
+
+            }
 
         }
     }
